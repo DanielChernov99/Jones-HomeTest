dbc138c4ededef2c25c0a970a4945d9659b3fa7c
Daniel Chernov <chernov.dani@gmail.com>
2026-06-23 21:54:29 +0300

    Run smoke test before regression tests

--- tests/smoke.spec.js
+++ tests/smoke.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test('should open the Jones test website', async ({ page }) => {
-  await page.goto('/');
+test('@smoke should open the Jones test website', async ({ page }) => {
+    await page.goto('/');
 
   await expect(
     page.getByRole('button', { name: /request a call back/i })
